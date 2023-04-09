c79564bcf1d6ef82b169d7a7d38e3c2926852601
Suren Dakshin <sdakshin@gmail.com>
2023-04-09 10:51:13 -0700

    initialcommit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee8c1d15c077ae89"; }
+/******/ 		__webpack_require__.h = function() { return "909c0e5a0dee330e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- components/Layout.js
+++ components/Layout.js
@@ -1,27 +1,58 @@
 
-import * as React from 'react'
-import Head from 'next/head'
+import * as React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import 'nProgress/nprogress.css';
+
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
+ 
+
 const Layout = (props) => {
     const head = () => (
+        <React.Fragment>
         <link 
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
         rel="stylesheet" 
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
         />   
-    );
+ 
+        <link 
+        rel="stylesheet"
+        href="/static/css/styles.css"
+        />
+
+       <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+        rel="stylesheet"
+        />
+        
+
+        </React.Fragment>
+     );
     const nav = () => (
 
-        <ul className="nav bg-warning">
+        <ul className="nav bg-warning ">
             <li className="nav-item">
-                <a className="nav-link text-dark" href=""> Home </a>
+                <Link className =  "nav-B"  href="/">
+                     Home 
+                </Link>                
             </li>
             <li className="nav-item">
-                <a className="nav-link text-dark" href=""> Login </a>
-            </li>
+                <Link className = "nav-B" href="/login">
+                    Login                    
+                </Link>                
+            </li>  
             <li className="nav-item">
-                <a className="nav-link text-dark" href=""> Register </a>
-            </li>                        
+                <Link className = "nav-B" href="/register">
+                    Register
+                </Link>                
+            </li>                       
+                    
 
         </ul>
 
